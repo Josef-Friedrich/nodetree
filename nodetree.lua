Josef-Friedrich/nodetree

commit f72d3b775499562e71f3850d219c186db23f3fd0

nodetree.lua (format.new_line): Do a line break after `\par{}`
There is no reason that the TeX channel output is a single, extremely long
line.

nodetree.lua
@@ -187,7 +187,7 @@ local format = {
     end
     local new_line
     if options.channel == 'tex' then
-      new_line = '\\par{}'
+      new_line = '\\par\n'
     else
       new_line = '\n'
     end

commit b52f911cad239dcc76ec9d71003d259949c8a807

nodetree.lua (compile_include): Replace newlines with `{}`
This saves us catcode games.

Fixes a bug introduced in commit f72d3b77549.

nodetree.lua
@@ -1681,7 +1681,10 @@ local export = {
     local include_file = assert(io.open(parent_path .. '/' .. example_counter .. '.nttex', 'r'))
     local include_content = include_file:read("*all")
     include_file:close()
-    include_content = include_content:gsub('[\r\n]', '')
+    -- To make the newline character be handled properly by the TeX engine
+    -- it would be necessary to set up its correct catcode.  However, it is
+    -- simpler to just replace all newlines with '{}'.
+    include_content = include_content:gsub('[\r\n]', '{}')
     tex.print(include_content)
   end,
 